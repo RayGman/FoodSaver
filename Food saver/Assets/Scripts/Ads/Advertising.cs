954121ee8d6816ab4a1f35bdffa68ae9df2c7a7a
Raygman <artmjeludkov@gmail.com>
2022-09-16 18:15:43 +0300

    Update Advertising.cs

--- Food saver/Assets/Scripts/Ads/Advertising.cs
+++ Food saver/Assets/Scripts/Ads/Advertising.cs
@@ -3,11 +3,6 @@ using GoogleMobileAds.Api;
 
 public class Advertising : MonoBehaviour
 {
-    //private const string appID = "ca-app-pub-3094727033726561~6503994050";
-    //private const string bannerID = "ca-app-pub-3094727033726561/6224792457";
-    //private const string interstitialID = "ca-app-pub-3094727033726561/5813728918";
-
-    //private BannerView banner;
     private InterstitialAd interstitialAd;
     private bool shown;
     private int lose;
@@ -33,16 +28,6 @@ public class Advertising : MonoBehaviour
         }
 
         lose++;
-        /*float randAd = Random.Range(0, 1);
-        if (randAd <= 0.3f)
-        {
-            BannerAdd();
-        }
-        else //if (randAd > 0.4f && randAd <= 1f)
-        {
-            InterstitialAdd();
-            shown = false;
-        } */
     }
 
     void Update()
@@ -53,18 +38,9 @@ public class Advertising : MonoBehaviour
         }
     }
 
-    /*private void BannerAdd()
-    {
-        string bannerID = "ca-app-pub-3094727033726561/6224792457";
-
-        banner = new BannerView(bannerID, AdSize.MediumRectangle, AdPosition.Center);
-        AdRequest adRequest = new AdRequest.Builder().Build();
-        banner.LoadAd(adRequest);
-    } */
-
     private void InterstitialAdd()
     {
-        string interstitialID = "ca-app-pub-3094727033726561/5813728918";
+        string interstitialID = "code";
 
         interstitialAd = new InterstitialAd(interstitialID);
         AdRequest adRequest = new AdRequest.Builder().Build();
